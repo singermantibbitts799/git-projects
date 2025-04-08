70e7c0fc84d2e9cc82b1ee4d33ff7963ac85c467
singermantibbitts799 <singermantibbitts799@gmail.com>
2025-04-08 05:26:18 +0000

    Add 28.ts

---
+++ src/28.ts
@@ -0,0 +1,12 @@
+let x = 10;
+console.log(x);
+
+const arr: number[] = [5, 7, 9];
+arr.push(2);
+console.log(arr);
+
+function addNumbers(a: number, b: number): number {
+    return a + b;
+}
+
+addNumbers(3, 4); // Call the function with two arguments
